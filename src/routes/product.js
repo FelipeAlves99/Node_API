c9f7f4d0dfae622706186c0a9299c026fac6eddf
FelipeAlves99 <falves99.dev@gmail.com>
2020-09-23 15:51:55 -0300

    Adicionado gets e refatorado put e delete

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,17 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/product");
 
-router.get("/", (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2",
-    });
-});
-
+router.get("/", controller.get);
+router.get("/:slug", controller.getBySlug);
+router.get("/admin/:id", controller.getById);
+router.get("/tags/:tag", controller.getByTag);
 router.post("/", controller.post);
-
 router.put("/:id", controller.put);
-
 router.delete("/", controller.delete);
 
 module.exports = router;
